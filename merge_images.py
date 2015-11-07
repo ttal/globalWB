54bca454ee3b3463828dd17d5143fce453d99a18
ttal <ttal@chegg.com>
2015-11-06 16:42:28 -0800

    v0.61
    updated image merging script

--- merge_images.py
+++ merge_images.py
@@ -5,30 +5,22 @@ import numpy as np
 
 class ImageMerge:
     def __init__(self, in_img1, in_img2):
-        img1 = cv2.imread(in_img1, cv2.CV_LOAD_IMAGE_UNCHANGED)
-        img2 = cv2.imread(in_img2, cv2.CV_LOAD_IMAGE_UNCHANGED)
-
-        self.img1_split = cv2.split(img1)
-        self.img2_split = cv2.split(img2)
-
-        im_dims = self.img1_split[0].shape
-        self.im_out_split = np.zeros((4, im_dims[0], im_dims[1]), np.uint8)
+        self.img1 = cv2.imread(in_img1)
+        self.img2 = cv2.imread(in_img2)
         self.im_out = None
 
+        im_dims = self.img1.shape
+        self.im_out_split = np.zeros((3, im_dims[0], im_dims[1]), np.uint8)
+
     def combine(self):
-        w_alpha1_only = (self.img1_split[3] == 255) & (self.img2_split[3] != 255)
-        w_alpha2_only = (self.img1_split[3] != 255) & (self.img2_split[3] == 255)
-        w_alpha_both = (self.img1_split[3] == 255) & (self.img2_split[3] == 255)
-        w_alpha_either = (self.img1_split[3] == 255) | (self.img2_split[3] == 255)
+
+        img1_split = cv2.split(self.img1)
+        img2_split = cv2.split(self.img2)
 
         for i in range(3):
-            self.im_out_split[i][w_alpha1_only] = self.img1_split[i][w_alpha1_only]
-            self.im_out_split[i][w_alpha2_only] = self.img2_split[i][w_alpha2_only]
-            self.im_out_split[i][w_alpha_both] = np.average([self.img1_split[i][w_alpha_both],
-                                                             self.img2_split[i][w_alpha_both]], axis=0)
-        self.im_out_split[3][w_alpha_either] = 255
+            self.im_out_split[i] = np.min([img1_split[i], img2_split[i]], axis=0)
 
-        self.im_out = cv2.merge(self.im_out_split, 4)
+        self.im_out = cv2.merge(self.im_out_split)
 
 
 if __name__ == '__main__':
